4baaaa49a8588106140a119ffe581fe68efb6641
Franco Duilmarc Chavez Ponce <franco.chp@gmail.com>
2021-01-07 15:53:01 -0500

    Add function FizzBuzz

--- chapter2.js
+++ chapter2.js
@@ -7,3 +7,29 @@ function LoopinATriangle( numberOfTimes )
         triangle+= '#';
     }
 }
+
+
+function fizzBuz(){
+    console.groupCollapsed('FizzBuzz First Method');
+    for( i = 1 ; i <= 100 ; ++i){
+        if( i % 3 !== 0 && i % 5 !== 0)
+            console.log(i);
+        else{
+            i % 3 ? console.log('Buzz') : console.log('fuzzy');
+        }   
+    }
+    console.groupEnd();
+}
+//Metod 2 
+function fizzBuz2(){
+    console.groupCollapsed('FizzBuzz Second Method');
+    let flag = true;
+    for( i = 1 ; i <= 100 ; ++i){
+        if( i % 3 === 0){
+            (i % 5 === 0) ? console.log(i,'fuzzyBuzz') : console.log(i,'fuzzy');
+        } 
+        else 
+            (i % 5 === 0) ? console.log(i,'Buzz') : console.log(i);
+    }
+    console.groupEnd();
+}
