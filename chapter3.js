4899ed2a566b2af72c2199ff6c5d85e16221e4d9
Franco Duilmarc Chavez Ponce <franco.chp@gmail.com>
2021-01-07 17:38:42 -0500

    add function isEven

--- chapter3.js
+++ chapter3.js
@@ -1,5 +1,23 @@
-
-function minimun(a,b){
-    (a-b>0) ? console.log(`minimun: ${b}`) : console.log(`minimun: ${a}`) 
+function minimun(number1,number2){
+    (number1-number2>0) ? console.log(`minimun: ${number2}`) : console.log(`minimun: ${number1}`) 
 }
 
+function isEven(number){
+    function recursive(number){
+        if(number < 0 ){
+            return null
+        }
+        else{
+            if(number===1){
+               return false
+            }
+            else
+            if(number === 0)
+                return true;
+            else{
+                return find(number-2)
+            }
+        }
+    }
+    return recursive(number);
+}
