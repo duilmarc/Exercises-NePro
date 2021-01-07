c27d2342e46d36e7d07a889b88fce52edd3a9aa5
Franco Duilmarc Chavez Ponce <franco.chp@gmail.com>
2021-01-07 15:17:14 -0500

    init exercise 1
    implements the function loopinATriangle

---
+++ chapter2.js
@@ -0,0 +1,9 @@
+function LoopinATriangle( numberOfTimes )
+{
+    let triangle = '#';
+    for( i = 0 ; i < numberOfTimes ; ++i)
+    {       
+        console.log(triangle);
+        triangle+= '#';
+    }
+}
