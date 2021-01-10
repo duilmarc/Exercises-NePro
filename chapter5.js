e6e7bac22ac4ce57ec2ab36e609de849ef6c90a9
Franco Duilmarc Chavez Ponce <franco.chp@gmail.com>
2021-01-09 20:43:49 -0500

    feat : Resolution of exercise 2

--- chapter5.js
+++ chapter5.js
@@ -1,16 +1,27 @@
 /// Exercise 1 
 
 /// array [ [ 1, 2, 3], [ 4 , 5 ] , [ 6] ] 
-
 // solution [ 1 , 2, 3 , 4 , 5 ,6]
-
 // take as 1 the groups
 
 
 let arrays = [[1, 2, 3], [4, 5], [6]];
 function flattening(array) {
-    return arrays.reduce( (array,currentarray ) => array.concat(currentarray)  ,[])
+    return arrays.reduce( (array,currentArray ) => array.concat(currentArray)  ,[])
 }
 
 console.log(arrays)
 console.log(flattening(arrays))
+
+/// Exercises 2 
+
+function ownLoop( value, testFunction, updateFunction, bodyfunction ){
+    while(true){
+        if ( !testFunction(value) ) break
+        bodyfunction(value)
+        value = updateFunction(value)
+    }
+
+}
+
+ ownLoop( 5 , n=>n>0 , n=>n-1, console.log)
