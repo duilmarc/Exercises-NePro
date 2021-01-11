533583e2251e1d208bdc42cafbda2f2bbea33631
Franco Duilmarc Chavez Ponce <franco.chp@gmail.com>
2021-01-11 13:07:37 -0500

    feat : solution to exercise 2

--- chapter6.js
+++ chapter6.js
@@ -19,10 +19,45 @@ class Vec{
         return new Vec(sumX,sumY)
     }
 }
-
+// Ejecution
 console.log(new Vec(1, 2).plus(new Vec(2, 3)));
 // → Vec{x: 3, y: 5}
 console.log(new Vec(1, 2).minus(new Vec(2, 3)));
 // → Vec{x: -1, y: -1}
 console.log(new Vec(3, 4).length);
 // → 5
+
+// Exercise 2
+class Group {
+    constructor( ){
+        this.elements = []
+    }
+
+    exists( value ){
+        return this.elements.indexOf(value)
+    }
+
+    has( value ){
+        if( this.exists(value) >= 0 )
+            return true
+        return false
+    }
+    add(value){
+        !this.has(value) ? this.elements.push(value) : null
+    }
+    delete(value){
+        let indexValue = this.exists(value) 
+        if( this.exists(value) >= 0 )
+            this.values =  this.elements.splice(indexValue, 1)
+        return false
+    }
+
+    static from(array) {
+        let group = new Group;
+        for (let value of array) {
+          group.add(value);
+        }
+        return group;
+      }
+  }
+  
