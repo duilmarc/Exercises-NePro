838b097dc0a47bba58b26dfe576ee52bf07f3c66
Franco Duilmarc Chavez Ponce <franco.chp@gmail.com>
2021-01-15 11:24:51 -0500

    feat: exercise 3

--- chapter9.js
+++ chapter9.js
@@ -59,3 +59,25 @@ console.log(text.replace(/(^|\W)'|'(\W|$)/g, '$1"$2'));
 // → "I'm the cook," he said, "it's my job."
 
 
+// exercise 3
+
+// Fill in this regular expression.
+let number = /^(-|\+)?(\d+(\.\d*)?|\.\d+)([eE](-|\+)?\d+)?$/;
+
+// Tests:
+for (let str of ["1", "-1", "+15", "1.55", ".5", "5.",
+                 "1.3e2", "1E-4", "1e+12"]) {
+  if (!number.test(str)) {
+    console.log(`Failed to match '${str}'`);
+  }
+  else{
+   console.log(`Accept to match '${str}'`);
+  }
+}
+for (let str of ["1a", "+-1", "1.2.3", "1+1", "1e4.5",
+                 ".5.", "1f5", "."]) {
+  if (number.test(str)) {
+    console.log(`Incorrectly accepted '${str}'`);
+  }
+}
+
