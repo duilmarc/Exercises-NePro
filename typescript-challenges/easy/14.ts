6ecd749024056b5ece58ac7ce93f09cbdc6b34e5
duilmarc <franco.chp@gmail.com>
2021-01-26 19:32:17 -0500

    exercise 14 typescript easy

---
+++ typescript-challenges/easy/14.ts
@@ -0,0 +1,8 @@
+type arr1 = ['a', 'b', 'c']
+type arr2 = [3, 2, 1]
+
+
+type First < T extends string[] | number[]> =  T[0]
+
+type head1 = First<arr1> // expected to be 'a'
+type head2 = First<arr2> // expected to be 3
