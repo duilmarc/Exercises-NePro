68508d565780652d2a8fe38055fc4b9178b886d4
duilmarc <franco.chp@gmail.com>
2021-01-26 19:45:44 -0500

    exercise 43 typescript easy

---
+++ typescript-challenges/easy/43.ts
@@ -0,0 +1,12 @@
+
+type MyExclude < T , U >  =  T extends U ? never : T
+
+
+type T0 = MyExclude<"a" | "b" | "c", "a">;
+//    ^ = type T0 = "b" | "c"
+type T1 = MyExclude<"a" | "b" | "c", "a" | "b">;
+//    ^ = type T1 = "c"
+
+
+type T2 = MyExclude<string | number | (() => void), Function>;
+//    ^ = type T2 = string | number
