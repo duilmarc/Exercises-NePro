016ea4dec2ede8a810ee86812731b43784016bb5
duilmarc <franco.chp@gmail.com>
2021-01-29 20:01:49 -0500

    problem 1 part 1 - js

--- intermediate-javascript-assessment/part1/assessment-part1.js
+++ intermediate-javascript-assessment/part1/assessment-part1.js
@@ -37,34 +37,33 @@ function daBears(){
   }
 }
 
-// Remove entries from the following arrays until only correct answers remain.
-// A correct answer will be an array of strings that are function names.
-
-// Which function(s) access the "chair" variable and get "Too Big!"
-// (Delete wrong answers, leave correct ones)
-
-var fairyTale1 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
-
-// Which function(s) access the "feeling" variable and get "Hungry"
-// (Delete wrong answers, leave correct ones)
-
-var fairyTale2 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
-
-// Which function(s) access the "porridge" variable and get "Too Cold!"
-// (Delete wrong answers, leave correct ones)
-
-var fairyTale3 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
-
-// Which function(s) access the "sleepy" variable and get undefined
-// (Delete wrong answers, leave correct ones)
-
-var fairyTale4 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
-
-// Which function(s) access the isFurry variable and get true
-// (Delete wrong answers, leave correct ones)
-
-var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
-
+  // Remove entries from the following arrays until only correct answers remain.
+  // A correct answer will be an array of strings that are function names.
+  
+  // Which function(s) access the "chair" variable and get "Too Big!"
+  // (Delete wrong answers, leave correct ones)
+  
+  var fairyTale1 = ["papaBear","mamaBear"];
+  
+  // Which function(s) access the "feeling" variable and get "Hungry"
+  // (Delete wrong answers, leave correct ones)
+  
+  var fairyTale2 = ["goldilocks"];
+  
+  // Which function(s) access the "porridge" variable and get "Too Cold!"
+  // (Delete wrong answers, leave correct ones)
+  
+  var fairyTale3 = [ "mamaBear"];
+  
+  // Which function(s) access the "sleepy" variable and get undefined
+  // (Delete wrong answers, leave correct ones)
+  
+  var fairyTale4 = ["daBears", "papaBear", "mamaBear", "goldilocks"];
+  
+  // Which function(s) access the isFurry variable and get true
+  // (Delete wrong answers, leave correct ones)
+  
+  var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear"];
 
 // *************
 // * PROBLEM 2 *
